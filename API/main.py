dbbc6c386fac6a256ea5d1c8392f9e2c13c1ab07
Somnath Shintre <somnath.shintre.acty77@gmail.com>
2022-12-21 22:55:27 +0530

    Card and Api to DB connectivity

--- API/main.py
+++ API/main.py
@@ -1,6 +1,41 @@
 from fastapi import FastAPI
+import os.path
+import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "RestuarntInfo.db")
 
 @app.get("/my-first-api")
 def hello():
   return {"Hello world!"}
+
+@app.get("/get-search-result")
+def get_search_(search, searchText: str = ""):
+  conn = sqlite3.connect(db_path)
+  cursor = conn.cursor()
+  if(searchText == ""):
+    cursor.execute("SELECT * FROM 'AllInfo' ORDER BY " + search + " ASC")
+    record = cursor.fetchall()
+    cursor.close()
+    return record
+  else:
+    query = "SELECT * FROM 'AllInfo' where " + search + "='" + searchText+"'"
+    cursor.execute(query)
+    record = cursor.fetchall()
+    cursor.close()
+    return record
--- WebApp/site.js
+++ WebApp/site.js
@@ -1,16 +1,61 @@
-$.ajax(
-  {
-    url: "http://127.0.0.1:8000/my-first-api", 
-    success: function(result)
-    {
-      console.log(result);
-    },
-    error: function(){
-      console.log("Error Occurred")
-    }
-  }
-);
+// $.ajax(
+//   {
+//     url: "http://127.0.0.1:8000/my-first-api", 
+//     success: function(result)
+//     {
+//       console.log(result);
+//     },
+//     error: function(){
+//       console.log("Error Occurred")
+//     }
+//   }
+// );
 
 $("#search").click(function(){
-    alert("The Search was clicked.");
-  });
+  var search = $('input[name=toggle]:checked').val();
+  var searchText = $('#searchText').val();
+  var parm;
+  if(searchText == ""){
+    parm = {
+      'search': search
+    }
+  }
+  else{
+    parm = {
+      search: search,
+      searchText: searchText
+    }
+  }
+  $.ajax(
+    {
+      url: "http://127.0.0.1:8000/get-search-result", 
+      dataType: "json",
+      data: parm,
+      success: function(result)
+      {
+        var newDiv = $(".row");
+        newDiv.empty();
+        if(result.length == 0){
+          newDiv.append("<h1>No data found relate to the search!</h1>")
+        }
+        for(var i=0; i<result.length; i++){
+          newDiv.append("<div class='col-sm-3' style='width: 18rem; height: 18rem;'>" + 
+                          "<div class='card'>" + 
+                            "<img class='card-img-top' src='https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000' alt='Card image cap'>" +
+                            "<div class='card-body'>" +
+                              "<h1 class='card-title'>"+result[i][1]+"</h1>" +
+                              "<p class='card-text'>Address"+result[i][6]+"</p>" +
+                              "<a href='"+result[i][8]+"' class='btn btn-primary' target='_blank'>Location</a>"+
+                            "</div>" +
+                          "</div>" +
+                        "</div>")
+        }
+      },
+      error: function()
+      {
+        console.log("Error Occurred")
+      }
+    }
+  );
+})
+  
